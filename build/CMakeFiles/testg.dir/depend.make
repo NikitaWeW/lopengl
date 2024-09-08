2380782d2fa513b08e1cea64fcf81b8733a9eeaa
NikitaWeW <nikita2510mart@gmail.com>
2024-09-08 14:57:49 +0300

    removed build filder

--- build/CMakeFiles/testg.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for testg.
-# This may be replaced when dependencies are built.
--- build/Vulkan-Headers/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/projects/learning-opengl/gvo/dependencies/vulkan/Vulkan-Headers
-# Build directory: D:/projects/learning-opengl/build/Vulkan-Headers
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
