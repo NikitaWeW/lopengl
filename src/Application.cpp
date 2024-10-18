32a404735a526850f241ef83ed917f33c0621582
NikitaWeW <nikita2510mart@gmail.com>
2024-10-18 15:53:26 +0300

    added imgui back finally

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,8 +1,15 @@
+/*
+all shitty bad initialization/uninitialization goes here
+*/
+
 #include <glad/gl.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 #include <logger.h>
 #include <stdexcept>
+#include <imgui.h>
+#include <backends/imgui_impl_opengl3.h>
+#include <backends/imgui_impl_glfw.h>
 
 #include "Application.hpp"
 #include "Renderer.hpp"
@@ -38,6 +45,17 @@ Application::Application() {
         LOG_FATAL("gladLoadGL: Failed to initialize GLAD!");
         throw std::runtime_error("gladLoadGL: Failed to initialize GLAD!");
     }
+    
+    ImGui::CreateContext();
+    IMGUI_CHECKVERSION();
+    ImGuiIO &io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // Enable Docking
+    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // Enable Multi-Viewport / Platform Windows
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
+    ImGui::StyleColorsDark();
 
     GLCALL(glEnable(GL_BLEND));
     GLCALL(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
