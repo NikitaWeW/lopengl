e4828f2110c6b729ff5ee9b645a9052334da4379
NikitaWeW <nikita2510mart@gmail.com>
2024-10-21 17:13:51 +0300

    glcall

---
+++ src/GlCall.h
@@ -0,0 +1,38 @@
+#include "logger.h"
+#include "glad/gl.h"
+#define GLCALL(x) \
+    x; \
+    while (GLenum GLerror = glGetError()) { \
+        char const *errorString;\
+        switch (GLerror) {\
+            case GL_NO_ERROR:\
+                errorString = "No error";\
+                break;\
+            case GL_INVALID_ENUM:\
+                errorString = "Invalid enum";\
+                break;\
+            case GL_INVALID_VALUE:\
+                errorString = "Invalid value";\
+                break;\
+            case GL_INVALID_OPERATION:\
+                errorString = "Invalid operation";\
+                break;\
+            case GL_STACK_OVERFLOW:\
+                errorString = "Stack overflow";\
+                break;\
+            case GL_STACK_UNDERFLOW:\
+                errorString = "Stack underflow";\
+                break;\
+            case GL_OUT_OF_MEMORY:\
+                errorString = "Out of memory";\
+                break;\
+            case GL_INVALID_FRAMEBUFFER_OPERATION:\
+                errorString = "Invalid framebuffer operation";\
+                break;\
+            default:\
+                errorString = "Unknown error";\
+                break;\
+            }\
+        LOG_ERROR("opengl error: %s", errorString); \
+    } 
+
