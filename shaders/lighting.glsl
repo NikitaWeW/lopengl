288d3e40596d14668fe7b0bab3a1acea09c1b377
nikita <nikita2510mart@gmail.com>
2025-01-31 17:40:36 +0300

    dynamic bias for dir light

--- shaders/lighting.glsl
+++ shaders/lighting.glsl
@@ -204,7 +204,7 @@ float calculateShadow(DirectionalLight light) {
     float closestDepth = texture(light.depthMap, projectedCoords.xy).r;
     float currentDepth = projectedCoords.z;
 
-    float bias = max(0.05 * (1.0 - dot(fs_in.v_normal, light.direction)), 0.005);
+    float bias = max(min(0.05, (1.0 - dot(fs_in.v_normal, light.direction))), 0.005);
     bias = 0.00001;
     return currentDepth - bias > closestDepth ? 1.0 : 0.0;
 }
