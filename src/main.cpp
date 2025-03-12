8f3a615ee44826c564e639347ae5208d8deaaefe
nikita <nikita2510mart@gmail.com>
2025-03-12 19:34:07 +0300

    back to CS

--- src/main.cpp
+++ src/main.cpp
@@ -43,7 +43,6 @@ cmake --build build && build/main
 #include "opengl/Framebuffer.hpp"
 #include "opengl/UniformBuffer.hpp"
 #include "opengl/Cubemap.hpp"
-#include "opengl/ShaderStorageBuffer.hpp"
 
 #include <chrono>
 #include <memory>
@@ -56,122 +55,61 @@ extern const bool debug = false;
 #else
 extern const bool debug = true;
 #endif
-#define SHOW_LOGS         true // for readability
-#define LOAD_NOW          true
-#define FLIP_TEXTURES     true
-#define FLIP_WINING_ORDER true
-#define SRGB              true
-#define currentShader app.shaders[app.displayShaders[app.currentShaderIndex]]
 
-void imguistuff(Application &app, ControllableCamera &cam);
-void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods);
-void scroll_callback(GLFWwindow *window, double xoffset, double yoffset);
+void imguistuff(Application &app);
+float lerp(float a, float b, float x) { return a + x * (b - a); }
 
 int main(int argc, char **argv)
 {
-    printf("loading...\n"); // TODO: cool progress bar
-    Application app;
-    GLFWwindow *window = app.window;
-    app.camera = ControllableCamera(window, {0, 0, 3}, {-90, 0, 0});
-    app.camera.far = 500;
-    Renderer renderer;
-    
+    Application app; // initialisation
+    app.camera = ControllableCamera{app.window, {0, 0, 0}, {0, -90, 0}};
     app.shaders = {
-        {"shaders/lighting.glsl",              SHOW_LOGS}, // 0
-    }; // on shader reload contents will be recompiled, if fails failed shader will be restored. 
+        {"shaders/raytracing.glsl", true},
+        {"shaders/hdr.glsl",        true}
+    };
 
-    Model cube{"res/models/cube.obj", !FLIP_TEXTURES, FLIP_WINING_ORDER};
-    Model quad{"res/models/quad.obj"};
+    Model quad{"res/models/quad.obj", false, false};
+    Texture mainTexture;
+    mainTexture.bind();
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    
+    glClearColor(0, 0, 0, 1);
+    constexpr unsigned numPerGroup = 20;
 
-    cube.getMeshes()[0].textures.push_back(Texture{"res/textures/oak.jpg", false, true, GL_CLAMP_TO_EDGE, GL_LINEAR, "diffuse"});
-
-//  ----------------------------------
-    // input here
-    constexpr unsigned numLights = 50;
-    constexpr unsigned numModels = 1000;
-    constexpr float radius = 50;
-    Model sceneModel = cube;
-//  ----------------------------------
-
-    glm::mat4 modelMatrices[numModels];
-    srand(static_cast<unsigned int>(glfwGetTime())); // initialize random seed
-    for(unsigned i = 0; i < numModels; ++i) {
-        glm::mat4 model{1.0f};
-        model = glm::translate(model, { randRange(-radius, radius), randRange(-radius, radius), randRange(-radius, radius) });
-        model = glm::scale(model, glm::vec3{ randRange(0.5f, 1.5f) });
-        model = glm::rotate(model, 1.0f, {randRange(0.0f, 360.0f), randRange(0.0f, 360.0f), randRange(0.0f, 360.0f)});
-        modelMatrices[i] = model;
-    }
-
-    SSBO matricesSSBO{numModels * sizeof(glm::mat4)};
-    glBufferSubData(GL_SHADER_STORAGE_BUFFER, 0, numModels * sizeof(glm::mat4), modelMatrices);
-
-    PointLight lights[numLights];
-    for(unsigned i = 0; i < numLights; ++i) {
-        PointLight light;
-        light.position = { randRange(-radius, radius), randRange(-radius, radius), randRange(-radius, radius) };
-        light.color = { randRange(0.0f, 2.0f), randRange(0.0f, 2.0f), randRange(0.0f, 2.0f) };
-        light.attenuation = randRange(0.07f, 0.09f);
-        lights[i] = light; // keep them in the memory
-        renderer.getLights().push_back(lights + i);
-    }
-
-// =========================== //
-
-
-    while (!glfwWindowShouldClose(window))
+    while (!glfwWindowShouldClose(app.window))
     {
         auto start = std::chrono::high_resolution_clock::now();
+        int prevWidth = -1, prevHeight = -1;
         app.camera.update(app.deltatime);
-        glfwGetWindowSize(window, &app.camera.width, &app.camera.height);
-
-        glViewport(0, 0, app.camera.width, app.camera.height);
-        renderer.clear(app.clearColor);
-
-        app.shaders[0].bind();
-        matricesSSBO.bind(0);
-        glShaderStorageBlockBinding(app.shaders[0].getRenderID(), app.shaders[0].getStorageBlock("modelSSBO"), 0);
-        
-        // set uniforms
-        glUniformMatrix4fv(app.shaders[0].getUniform("u_viewMat"),      1, GL_FALSE, &app.camera.getViewMatrix()[0][0]);
-        glUniformMatrix4fv(app.shaders[0].getUniform("u_projectionMat"),1, GL_FALSE, &app.camera.getProjectionMatrix()[0][0]);
-        glUniform3fv(app.shaders[0].getUniform("u_viewPos"), 1, &app.camera.position.x);
-        renderer.setLightingUniforms(app.shaders[0]);
-
-        for(Mesh const &mesh : sceneModel.getMeshes()) {
-            bool specularSet = false;
-            bool normalSet = false;
-            bool heightSet = false;
-            unsigned int textureCount = 0;
-            for(Texture const &texture : mesh.textures) {
-                int location = app.shaders[0].getUniform("u_material." + texture.type);
-                if(location != -1) {
-                    glUniform1i(location, textureCount);
-                    texture.bind(textureCount);
-                    ++textureCount;
-                }
-                if(texture.type == "specular") {
-                    specularSet = true;
-                } else if(texture.type == "normal") {
-                    normalSet = true;
-                } else if(texture.type == "height") {
-                    heightSet = true;
-                }
-            }
-
-            glUniform1i(app.shaders[0].getUniform("u_material.specularSet"), specularSet);
-            glUniform1i(app.shaders[0].getUniform("u_material.normalSet"), normalSet);
-            glUniform1i(app.shaders[0].getUniform("u_material.heightSet"), heightSet);
-         
-            
-            mesh.va.bind();
-            mesh.ib.bind();
-            glDrawElementsInstanced(GL_TRIANGLES, mesh.ib.getSize(), GL_UNSIGNED_INT, nullptr, numModels);
+        glfwGetWindowSize(app.window, &app.camera.width, &app.camera.height);
+        if(prevWidth != app.camera.width || prevHeight != app.camera.height) { // resize textures
+            mainTexture.bind();
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, app.camera.width, app.camera.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
         }
+//  =========================================== 
+        glViewport(0, 0, app.camera.width, app.camera.height);
+        app.shaders[0].bind();
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+        glUniform1i(app.shaders[0].getUniform("u_output"), 0);
+        glBindImageTexture(0, mainTexture.getRenderID(), 0, GL_FALSE, 0, GL_READ_WRITE, GL_RGBA16F);
+        glDispatchCompute(app.camera.width / numPerGroup + 1, app.camera.height / numPerGroup + 1, 1);
 
+        app.shaders[1].bind();
+        glUniform1i(app.shaders[0].getUniform("u_texture"), 0);
+        mainTexture.bind(0);
+        quad.getMeshes()[0].va.bind();
+        quad.getMeshes()[0].ib.bind();
+        glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
+        glDrawElements(GL_TRIANGLES, quad.getMeshes()[0].ib.getSize(), GL_UNSIGNED_INT, nullptr);
 
-        if(app.frameCounter % 100 == 0) glfwSetWindowTitle(window, ("lopengl -- " + std::to_string((int) glm::round(1 / app.deltatime)) + " FPS").c_str());
-        glfwSwapBuffers(window);
+//  =========================================== 
+        imguistuff(app);
+        if(app.frameCounter % 100 == 0) glfwSetWindowTitle(app.window, ("lopengl -- " + std::to_string((int) glm::round(1 / app.deltatime)) + " FPS").c_str());
+        glfwSwapBuffers(app.window);
         glfwPollEvents();
         ++app.frameCounter;
         app.deltatime = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - start).count() * 1.0E-6;
