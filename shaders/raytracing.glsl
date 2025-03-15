5ef19c38f28ec5d06dd4271a0fca410499004770
nikita <nikita2510mart@gmail.com>
2025-03-15 17:28:05 +0300

    fixed ray-sphere intersection test

--- shaders/raytracing.glsl
+++ shaders/raytracing.glsl
@@ -24,9 +24,10 @@ struct ShpereRayCollision {
     bool isCollide;
     vec3 location;
 };
+ShpereRayCollision raySphere(Ray ray, Sphere sphere);
 
 vec3 rayColor(Ray ray);
-ShpereRayCollision raySphere(Ray ray, Sphere sphere);
+Ray calculateRay(vec2 texCoords, Camera camera);
 
 Sphere testSphere = Sphere(vec3(0,0,-4), 1);
 
@@ -34,28 +35,41 @@ void main() {
     ivec2 texelCoord = ivec2(gl_GlobalInvocationID.xy);
     vec2 texCoords = vec2(texelCoord) / (gl_NumWorkGroups.xy*gl_WorkGroupSize.xy);
 
-    vec2 texCoordsNDC = texCoords * 2 - 1;
-    float tangentFOV = tan(radians(u_camera.fov) * 0.5);
-    vec2 offset = vec2(texCoordsNDC.x * u_camera.aspect * tangentFOV, texCoordsNDC.y * tangentFOV);
-    Ray ray = Ray(normalize(u_camera.forward + offset.x * u_camera.right + offset.y * u_camera.up), u_camera.position);
-
+    Ray ray = calculateRay(texCoords, u_camera);
     imageStore(u_output, texelCoord, vec4(rayColor(ray), 1.0));
 }
 
 vec3 rayColor(Ray ray) {
+    // return ray.origin;
     ShpereRayCollision sphereCollision = raySphere(ray, testSphere);
     if(sphereCollision.isCollide) {
-        return normalize(sphereCollision.location - testSphere.center);
+        vec3 normal = normalize(sphereCollision.location - testSphere.center);
+        return sphereCollision.location;
     } else {
         return (1 - normalize(ray.direction).y) * vec3(0.3, 0.5, 0.7);
     }
 }
 ShpereRayCollision raySphere(Ray ray, Sphere sphere) {
-    vec3 OC = sphere.center - ray.origin;
-    float a = dot(ray.direction, ray.direction);
-    float b = -2.0 * dot(ray.direction, OC);
-    float c = dot(OC, OC) - sphere.radius * sphere.radius;
-    float discriminant = b*b - 4*a*c;
+    const vec3 OC = sphere.center - ray.origin;
+    const float a = dot(ray.direction, ray.direction);
+    const float b = -2.0 * dot(ray.direction, OC);
+    const float c = dot(OC, OC) - sphere.radius * sphere.radius;
+    const float discriminant = b*b - 4*a*c;
     if(discriminant < 0) return ShpereRayCollision(false, vec3(0));
-    return ShpereRayCollision(true, ray.origin + ((-b - sqrt(discriminant)) / 2*a) * normalize(ray.direction));
+    
+    const float sqrtDiscriminant = sqrt(discriminant);
+    float t = min((-b - sqrtDiscriminant) / (2*a), (-b + sqrtDiscriminant) / (2*a));
+
+    if(t < 0) return ShpereRayCollision(false, vec3(0));
+
+    return ShpereRayCollision(true, ray.origin + t * normalize(ray.direction));
+}
+Ray calculateRay(vec2 texCoords, Camera camera) {
+    vec2 NDCcoords = texCoords * 2.0 - 1.0;
+    float nearPlaneScale = tan(radians(camera.fov) * 0.5);
+    vec2 viewPortCoords = vec2(NDCcoords.x * camera.aspect * nearPlaneScale, NDCcoords.y * nearPlaneScale);
+
+    const vec3 rayDir = camera.forward + viewPortCoords.x * camera.right + viewPortCoords.y * camera.up;
+
+    return Ray(normalize(rayDir), camera.position);
 }
