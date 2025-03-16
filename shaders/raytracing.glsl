9041c5efdc17ae7b4bcdd2e2817c25700cc727ee
nikita <nikita2510mart@gmail.com>
2025-03-16 13:09:28 +0300

    aabb ray test

--- shaders/raytracing.glsl
+++ shaders/raytracing.glsl
@@ -16,27 +16,31 @@ struct Ray {
     vec3 direction;
     vec3 origin;
 };
+struct PointIntersection {
+    bool exists;
+    vec3 location;
+};
+struct BoolIntersection {
+    bool exists;
+};
 struct Sphere {
     vec3 center;
     vec3 color;
     float radius;
 };
-struct SphereRayCollision {
-    bool exists;
-    vec3 location;
-};
-SphereRayCollision raySphere(Ray ray, Sphere sphere);
-
 struct Triangle {
     vec3 A;
     vec3 B;
     vec3 C;
 };
-struct RayTriangleIntersection {
-    bool exists;
-    vec3 location;
+struct AABB {
+    vec3 min;
+    vec3 max;
 };
-RayTriangleIntersection rayTriangle(Ray ray, Triangle triangle);
+
+PointIntersection raySphere(Ray ray, Sphere sphere);
+PointIntersection rayTriangle(Ray ray, Triangle triangle);
+PointIntersection rayAABB(Ray ray, AABB aabb);
 vec3 getBarycentric(vec3 p, vec3 a, vec3 b, vec3 c);
 
 uniform float u_time;
@@ -44,15 +48,14 @@ uniform float u_time;
 vec3 rayColor(Ray ray);
 Ray calculateRay(vec2 texCoords, Camera camera);
 
-Sphere testSphere = Sphere(
-    vec3(0,0,-4), 
-    vec3(0.7, 0.4, 0.2), 
-    1
-);
 Triangle testTriangle = Triangle(
     vec3(-3,  1, -4),
     vec3( 1,  5, -3),
     vec3( 4, -1, -5)
+);
+AABB testAABB = AABB(
+    vec3(-1, -1, -4),
+    vec3(1, 1, -2)
 );
 
 void main() {
@@ -64,27 +67,24 @@ void main() {
 }
 
 vec3 rayColor(Ray ray) {
-    RayTriangleIntersection triangleIntersection = rayTriangle(ray, testTriangle);
-    if(triangleIntersection.exists) {
-        return normalize(triangleIntersection.location);
-    } else {
-        return vec3(0.2, 0.3, 0.7) * normalize(1 - ray.direction).y;
-    }
+    PointIntersection intersection = rayAABB(ray, testAABB);
+    float exists = float(intersection.exists);
+    return exists * intersection.location + (1 - exists) * vec3(0.2, 0.3, 0.7) * normalize(1 - ray.direction).y;
 }
-SphereRayCollision raySphere(Ray ray, Sphere sphere) {
+PointIntersection raySphere(Ray ray, Sphere sphere) {
     const vec3 OC = sphere.center - ray.origin;
     const float a = dot(ray.direction, ray.direction);
     const float h = dot(ray.direction, OC);
     const float c = dot(OC, OC) - sphere.radius * sphere.radius;
     const float discriminant = h*h - a*c;
-    if(discriminant < 0) return SphereRayCollision(false, vec3(0));
+    if(discriminant < 0) return PointIntersection(false, vec3(0));
     
     const float sqrtDiscriminant = sqrt(discriminant);
     float t = min((h - sqrtDiscriminant) / a, (h + sqrtDiscriminant) / a);
 
-    if(t < 0) return SphereRayCollision(false, vec3(0));
+    if(t < 0) return PointIntersection(false, vec3(0));
 
-    return SphereRayCollision(true, ray.origin + t * normalize(ray.direction));
+    return PointIntersection(true, ray.origin + t * normalize(ray.direction));
 }
 Ray calculateRay(vec2 texCoords, Camera camera) {
     vec2 NDCcoords = texCoords * 2.0 - 1.0;
@@ -95,19 +95,12 @@ Ray calculateRay(vec2 texCoords, Camera camera) {
 
     return Ray(normalize(rayDir), camera.position);
 }
-/*
-plane equation: n * (a - p) = 0
-ray equation: o + d * t
-n * (o + d * t - p) = 0
-solve for t:
-t = (n * (p - o)) / (n*d)
-*/
-RayTriangleIntersection rayTriangle(Ray ray, Triangle triangle) {
+PointIntersection rayTriangle(Ray ray, Triangle triangle) {
     const vec3 normal = normalize(cross(triangle.B - triangle.A, triangle.C - triangle.A)); // change
     const float denominator = dot(normal, ray.direction);
-    if(denominator == 0) return RayTriangleIntersection(false, vec3(0));
+    if(denominator == 0) return PointIntersection(false, vec3(0));
     const float t = dot(normal, triangle.A - ray.origin) / denominator;
-    if(t < 0) return RayTriangleIntersection(false, vec3(0));
+    if(t < 0) return PointIntersection(false, vec3(0));
     const vec3 planePoint = ray.origin + ray.direction * t;
 
     vec3 barycentric = getBarycentric(planePoint, triangle.A, triangle.B, triangle.C);
@@ -116,10 +109,9 @@ RayTriangleIntersection rayTriangle(Ray ray, Triangle triangle) {
         0 <= barycentric.x && barycentric.x <= 1 && 
         0 <= barycentric.y && barycentric.y <= 1 &&
         0 <= barycentric.z && barycentric.z <= 1
-    ) return RayTriangleIntersection(true, planePoint);
-    return RayTriangleIntersection(false, vec3(0));
+    ) return PointIntersection(true, planePoint);
+    return PointIntersection(false, vec3(0));
 }
-
 vec3 getBarycentric(vec3 p, vec3 a, vec3 b, vec3 c) {
     const vec3 v0 = b - a;
     const vec3 v1 = c - a;
@@ -129,4 +121,21 @@ vec3 getBarycentric(vec3 p, vec3 a, vec3 b, vec3 c) {
     const float w = (v0.x * v2.y - v2.x * v0.y) / denominator;
     const float u = 1.0f - v - w;
     return vec3(v, w, u);
+}
+PointIntersection rayAABB(Ray ray, AABB aabb) {
+    vec3 dirfrac = 1.0f / ray.direction;
+    float t1 = (aabb.min.x - ray.origin.x)*dirfrac.x;
+    float t2 = (aabb.max.x - ray.origin.x)*dirfrac.x;
+    float t3 = (aabb.min.y - ray.origin.y)*dirfrac.y;
+    float t4 = (aabb.max.y - ray.origin.y)*dirfrac.y;
+    float t5 = (aabb.min.z - ray.origin.z)*dirfrac.z;
+    float t6 = (aabb.max.z - ray.origin.z)*dirfrac.z;
+
+    float tmin = max(max(min(t1, t2), min(t3, t4)), min(t5, t6));
+    float tmax = min(min(max(t1, t2), max(t3, t4)), max(t5, t6));
+
+    // if tmax < 0, ray (line) is intersecting AABB, but the whole AABB is behind us
+    // if tmin > tmax, ray doesn't intersect AABB
+    if (tmax < 0 || tmin > tmax) return PointIntersection(false, vec3(0));
+    return PointIntersection(true, ray.origin + tmin * normalize(ray.direction));
 }
--- src/main.cpp
+++ src/main.cpp
@@ -79,6 +79,8 @@ int main(int argc, char **argv)
     glClearColor(0, 0, 0, 1);
     constexpr unsigned numPerGroup = 20;
 
+    std::thread showFps([&app](){ while(!glfwWindowShouldClose(app.window)) { std::this_thread::sleep_for(std::chrono::milliseconds(1000)); glfwSetWindowTitle(app.window, ("lopengl -- " + std::to_string((int) glm::round(1 / app.deltatime)) + " FPS").c_str()); }});
+
     while (!glfwWindowShouldClose(app.window))
     {
         auto start = std::chrono::high_resolution_clock::now();
@@ -115,12 +117,12 @@ int main(int argc, char **argv)
 
 //  =========================================== 
         imguistuff(app);
-        if(app.frameCounter % 100 == 0) glfwSetWindowTitle(app.window, ("lopengl -- " + std::to_string((int) glm::round(1 / app.deltatime)) + " FPS").c_str());
         glfwSwapBuffers(app.window);
         glfwPollEvents();
         ++app.frameCounter;
         app.deltatime = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - start).count() * 1.0E-6;
     }
+    showFps.join();
 }
 /*
 c++ be like:
